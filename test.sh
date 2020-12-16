4c13ad66e99c2ec0b264e35d43ffa514c91367be
wangyueshenga <2719464332@qq.com>
2020-12-16 12:51:21 +0000

    still have some bug in the program

---
+++ test.sh
@@ -0,0 +1,71 @@
+echo "清理历史编译结果"
+var_1=`make clean`
+echo ${var_1}
+echo
+echo "执行编译结果"
+var_2=`make`
+echo ${var_2}
+echo
+echo "test01"
+var_3=`make test01`
+echo ${var_3}
+echo
+echo "test02"
+var_4=`make test02`
+echo ${var_4}
+echo
+echo "test03"
+var_5=`make test03`
+echo ${var_5}
+echo
+echo "test04"
+var_6=`make test04`
+echo ${var_6}
+echo
+echo "test05"
+var_7=`make test05`
+echo ${var_7}
+echo
+echo "test06"
+var_8=`make test06`
+echo ${var_8}
+echo
+echo "test07"
+var_9=`make test07`
+echo ${var_9}
+echo
+echo "test08"
+var_10=`make test08`
+echo ${var_10}
+echo
+echo "test09"
+var_11=`make test09`
+echo ${var_11}
+echo
+echo "test10"
+var_12=`make test10`
+echo ${var_12}
+echo
+echo "test11"
+var_13=`make test11`
+echo ${var_13}
+echo
+echo "test12"
+var_14=`make test12`
+echo ${var_14}
+echo
+echo "test13"
+var_15=`make test13`
+echo ${var_15}
+echo
+echo "test14"
+var_16= `make test14`
+echo ${var_16}
+echo
+echo "test15"
+var_17=`make test15`
+echo ${var_17}
+echo
+echo "test16"
+var_18=`make test16`
+echo ${var_18}
